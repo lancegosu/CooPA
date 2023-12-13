2f7adaa50437a4c6bbc53c666113c5a36024f0e0
Lance <106897295+lancerai@users.noreply.github.com>
2023-12-13 11:05:13 -0800

    Update coopa.py

--- coopa.py
+++ coopa.py
@@ -3,15 +3,21 @@ from utils import smart_search, grab_urls, get_citation
 
 app = Flask(__name__)
 
+# Route for the home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Route for handling form submission
 @app.route('/submit', methods=['POST'])
 def submit():
-    user_input = request.form['user_input1']
+    user_input = request.form['user_input1']  # Retrieve user input from the form
+    
+    # Use the smart_search function to get relevant information
+    # and grab_urls to extract URLs related to the user input
     urls = grab_urls(user_input, num_link=4)
     text = smart_search(user_input)
+    
     citation = get_citation(urls)
     result = f"{text}\n\nSources:\n{citation}"
     return result
