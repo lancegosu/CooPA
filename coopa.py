fc13554b08f6281d8a22b2c5797a423189247f14
Lance <106897295+lancerai@users.noreply.github.com>
2023-12-17 14:30:31 -0800

    Update coopa.py

--- coopa.py
+++ coopa.py
@@ -11,7 +11,12 @@ def home():
 # Route for handling form submission
 @app.route('/submit', methods=['POST'])
 def submit():
-    user_input = request.form['user_input1']  # Retrieve user input from the form
+    user_input = request.form['user_input1'].strip()  # Retrieve user input from the form
+    
+    # Check if the user input is empty
+    if not user_input:
+        return "Please provide a valid input."
+        
     urls = grab_urls(user_input, num_link=4) # Extract URLs related to the user input
     text = smart_search(user_input) # Get relevant information
     citation = get_citation(urls)
